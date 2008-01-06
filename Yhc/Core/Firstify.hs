c9459f7266318a98eb3868307013a99eeffbed4d
Neil Mitchell <ndmitchell@gmail.com>
2008-01-06 00:56:19 +0000

    Add a dummy Firstify module with a basic interface

---
+++ Yhc/Core/Firstify.hs
@@ -0,0 +1,12 @@
+
+module Yhc.Core.Firstify where
+
+
+
+reynolds :: Core -> Core
+reynolds = error "Yhc.Core.Firstify.reynolds: todo"
+
+
+
+firstify :: Core -> Core
+firstify = error "Yhc.Core.Firstify.firstify: todo"
