c33c149a6146dfe214d570d9de993a0e28f2c653
Neil Mitchell <ndmitchell@gmail.com>
2008-01-12 05:03:08 +0000

    Implement most of Reynold's style defunctionalisation

--- Yhc/Core/Firstify.hs
+++ Yhc/Core/Firstify.hs
@@ -6,16 +6,73 @@ import Yhc.Core.FreeVar3
 import Yhc.Core.UniqueId
 
 import Control.Monad.State
+import Data.Char
+import Data.List
 import qualified Data.Homeomorphic as H
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 
 ---------------------------------------------------------------------
 -- REYNOLDS METHOD
 
 reynolds :: Core -> Core
-reynolds = error "Yhc.Core.Firstify.reynolds: todo"
+reynolds c = c3
+    where
+        -- set up some information
+        c2 = transformExpr appRules c
+        arr = Map.fromList [(coreFuncName x, coreFuncArity x) | x <- coreFuncs c]
+        apFun = findApFun c
+        apTyp = findApTyp c
+        
+        a <#> b | isDigit (last a) = a ++ "_" ++ show b
+                | otherwise = a ++ show b
 
+        appRules (CoreFun x) = CoreApp (CoreFun x) []
+        appRules (CoreApp x []) | not $ isCoreFun x = x
+        appRules (CoreApp (CoreApp x y) z) = CoreApp x (y++z)
+        appRules x = x
+
+        -- just transform the thing
+        c3 = transformExpr defunc c2
+
+        defunc (CoreApp (CoreFun x) xs) =
+            case compare (length xs) a of
+                EQ -> CoreApp (CoreFun x) xs
+                LT -> ap_ x xs
+                GT -> ap (CoreApp (CoreFun x) yes) no
+                    where (yes,no) = splitAt a xs
+            where a = arr Map.! x
+        defunc (CoreApp x xs) | not $ isCoreCon x = ap x xs
+        defunc x = x
+
+        ap  fun args = CoreApp (CoreFun name) (fun:args)
+            where
+                name = if n == 1 then apFun else apFun <#> n
+                n = length args
+        
+        ap_ fun args = CoreApp (CoreCon $ name ++ "_" ++ fun) args
+            where
+                name = if n == 0 then apTyp else apTyp <#> n
+                n = length args
+
+        -- then figure out which functions we required
+
+
+findApFun :: Core -> CoreFuncName
+findApFun c = findName (map coreFuncName $ coreFuncs c) "ap"
+
+findApTyp :: Core -> String
+findApTyp c = findName (concatMap f $ coreDatas c) "Ap"
+    where f x = coreDataName x : map coreCtorName (coreDataCtors x)
+
+-- find a name pre# (where # is blank or a number)
+-- such that pre# is not a prefix of any of the seen set
+findName :: [String] -> String -> String
+findName seen pre = if null seen2 then pre else pre ++ show (head $ filter isValid [1..])
+    where
+        isValid i = not $ any ((pre ++ show i) `isPrefixOf`) seen2
+        seen2 = filter (pre `isPrefixOf`) seen
 
 
 ---------------------------------------------------------------------
