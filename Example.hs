ff86ab1875b21e0fe7708a59a84612d9ae75bb6b
Neil Mitchell <ndmitchell@gmail.com>
2008-01-25 23:22:56 +0000

    Add another example

--- Example.hs
+++ Example.hs
@@ -17,11 +17,15 @@ main x y = neq (x::Int) y
 neq x y = not (x == y)
 -}
 
+{-
 main xs = (a . b . c) xs
-
 a x = x+(1::Int)
 b x = x*(4::Int)
 c x = x-(3::Int)
+-}
+
+main x = even (x :: Int)
+
 
 
 
