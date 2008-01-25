9cd9e375e56fa4f896ef3ea7630914e4f6e13e69
Neil Mitchell <ndmitchell@gmail.com>
2008-01-25 22:35:07 +0000

    Add another example, which tests the homeomorphic embedding

--- Example.hs
+++ Example.hs
@@ -12,5 +12,16 @@ neq = not . eq
 eq x = x == (0::Int)
 -}
 
+{-
 main x y = neq (x::Int) y
 neq x y = not (x == y)
+-}
+
+main xs = (a . b . c) xs
+
+a x = x+(1::Int)
+b x = x*(4::Int)
+c x = x-(3::Int)
+
+
+
