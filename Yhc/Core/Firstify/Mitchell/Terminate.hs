ffdb2cb34559deed80c677ccdb9b79e98744aebe
Neil Mitchell <ndmitchell@gmail.com>
2008-02-04 00:06:17 +0000

    Implement most of the specialisation stuff

--- Yhc/Core/Firstify/Mitchell/Terminate.hs
+++ Yhc/Core/Firstify/Mitchell/Terminate.hs
@@ -1,36 +1,63 @@
 
-module Yhc.Core.Firstify.Mitchell.Terminate where
+module Yhc.Core.Firstify.Mitchell.Terminate(
+    Terminate, emptyTerminate,
+    addInline, askInline
+    ) where
 
-import Yhc.Core
 import qualified Data.Homeomorphic as H
+import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Debug.Trace
+import Yhc.Core
+import Yhc.Core.Firstify.Mitchell.Util
 
 
 data Terminate = Terminate
+    {verbose :: Bool
+    ,terminate :: Map.Map CoreFuncName Term
+    }
+
+data Term = Term
     {specs :: H.Homeomorphic CoreExpr1 CoreExpr
-    ,specsDone :: [(H.Shell CoreExpr1,CoreExpr)]
     ,inlined :: Set.Set CoreFuncName
     }
 
 
-mergeTerminate :: Terminate -> Terminate -> Terminate
-mergeTerminate t1 t2 = foldl (flip $ uncurry addSpec)
-    t1{inlined = inlined t1 `Set.union` inlined t2} (specsDone t2)
+get name t = Map.findWithDefault emptyTerm name (terminate t)
+modify t name op = t{terminate = Map.insert name (op $ get name t) (terminate t)}
+
+logger t msg answer = (if verbose t && not answer then trace msg else id) answer
 
 
-addSpec :: H.Shell CoreExpr1 -> CoreExpr -> Terminate -> Terminate
-addSpec shell value t = t{specsDone = (shell,value) : specsDone t
-                          ,specs = H.insert shell value (specs t)}
+emptyTerminate :: Bool -> Terminate
+emptyTerminate b = Terminate b Map.empty
 
 
-allowSpec :: H.Shell CoreExpr1 -> Terminate -> Bool
-allowSpec x t = length (H.find x (specs t)) <= 1
+emptyTerm :: Term
+emptyTerm = Term H.empty Set.empty
 
 
-addInline :: CoreFuncName -> Terminate -> Terminate
-addInline x t = t{inlined = Set.insert x (inlined t)} 
+addInline :: Terminate -> CoreFuncName -> CoreFuncName -> Terminate
+addInline t within on = modify t within $ \x -> x{inlined = Set.insert on $ inlined x}
 
 
-allowInline :: CoreFuncName -> Terminate -> Bool
-allowInline x t = not $ x `Set.member` inlined t
+askInline :: Terminate -> CoreFuncName -> CoreFuncName -> Bool
+askInline t within on = logger t ("Skipped inlining of: " ++ on ++ " within " ++ within) $
+    on `Set.notMember` inlined (get within t)
 
+
+addSpec :: Terminate -> CoreFuncName -> CoreExpr -> Terminate
+addSpec t within on = modify t within $ \x -> x{specs = H.insert (specKey on) on $ specs x}
+
+specKey = shellify . blurVar
+
+
+askSpec :: Terminate -> CoreFuncName -> CoreExpr -> Bool
+askSpec t within on = logger t ("Skipped spec of: " ++ show on) $
+    length (H.find (specKey on) $ specs $ get within t) <= 1
+
+
+cloneSpec :: Terminate -> CoreFuncName -> CoreFuncName -> Terminate
+cloneSpec t from to = case Map.lookup from (terminate t) of
+                           Nothing -> t
+                           Just y -> t{terminate = Map.insert to y{inlined=Set.empty} $ terminate t}
