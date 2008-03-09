98a1d7b6d3e85435edb74bfa9f8f8a5329916836
Neil Mitchell <ndmitchell@gmail.com>
2008-03-09 15:17:36 +0000

    Add a script to run the entire nofib suite

---
+++ run-nofib.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+if not "%1" == "" goto continue
+for %%i in (imaginary) do for %%j in (yca\%%i\*.yca) do call %0 %%j
+goto end
+
+:continue
+echo Processing %1
+firstify %1 -himl > %1.txt 2>&1
+
+:end
