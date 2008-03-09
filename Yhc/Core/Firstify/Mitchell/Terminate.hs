8a6648f48aafc3de8c647f8dd205acb6a1671f44
Neil Mitchell <ndmitchell@gmail.com>
2008-03-09 16:46:36 +0000

    Change homeoOrder to 20, should be massive for most programs (do need to figure out a sensible bound on this)

--- Yhc/Core/Firstify/Mitchell/Terminate.hs
+++ Yhc/Core/Firstify/Mitchell/Terminate.hs
@@ -25,7 +25,7 @@ data Term = Term
     }
 
 
-homeoOrder = 4 :: Int
+homeoOrder = 20 :: Int
 
 insertH key val [] = error "Logic fault, insertH"
 insertH key val (x:xs) | isNothing (H.findOne key x) = H.insert key val x : xs
