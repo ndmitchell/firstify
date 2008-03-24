88f8f08ef6718507b2131b8b8790bc4dcaf9cdfe
Neil Mitchell <ndmitchell@gmail.com>
2008-03-24 17:51:27 +0000

    Add a tex2hs checking file

---
+++ academic/icfp2008/Include.hs
@@ -0,0 +1,21 @@
+
+import Prelude hiding (map)
+import Data.List hiding (map)
+
+-- # do
+-- # c ; v ; xs ; y ; v_1 ; i
+-- # IO ; Double
+-- # Show
+-- # primCatch
+-- # _s ; _b ; _a ; _e
+-- # _l' ; _b'
+-- # lam ; case_ ; let_
+
+data World = World
+
+y = undefined
+x = undefined
+g = undefined
+
+expensive = undefined
+
