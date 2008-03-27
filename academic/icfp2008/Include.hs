902525b3d2f169142c65de00563928673d3abcf5
Neil Mitchell <ndmitchell@gmail.com>
2008-03-27 11:34:10 +0000

    Add stuff to get the type checker successfully running over it

--- academic/icfp2008/Include.hs
+++ academic/icfp2008/Include.hs
@@ -3,19 +3,22 @@ import Prelude hiding (map)
 import Data.List hiding (map)
 
 -- # do
--- # c ; v ; xs ; y ; v_1 ; i
+-- # c ; v ; y ; v_1 ; i
 -- # IO ; Double
 -- # Show
 -- # primCatch
 -- # _s ; _b ; _a ; _e
 -- # _l' ; _b'
 -- # lam ; case_ ; let_
+-- instance Eq Expr
 
 data World = World
 
 y = undefined
 x = undefined
 g = undefined
+e = undefined
+xs = undefined
 
 expensive = undefined
 
